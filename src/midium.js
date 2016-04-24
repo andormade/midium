d7c4af44b8a82a1d8fc08d35a15ac20b929eb2ac
Andor Polgar <hngrhorace@gmail.com>
2016-04-24 22:02:04 +0200

    Removed buffering and put it into the core package

--- src/midium.js
+++ src/midium.js
@@ -7,21 +7,4 @@ export default class Midium extends MidiumCore {
 		super(query);
 		this.defaultChannel = DEFAULT_CHANNEL;
 	}
-
-	send(message, delay) {
-		if (this.isBuffering) {
-			this.buffer.push({
-				message : message,
-				delay   : delay
-			});
-		}
-		else if (typeof delay === "number") {
-			super.send(message, delay + window.performance.now());
-		}
-		else {
-			super.send(message);
-		}
-
-		return this;
-	}
 }
